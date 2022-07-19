382cada06de8d615f3e8804d70169e277f76379c
Luca Burgazzoli <lburgazzoli@gmail.com>
2022-07-19 13:27:20 +0200

    docker

--- etc/scripts/build-images.sh
+++ etc/scripts/build-images.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+DIRS=$(find . -name "connectors" -type d -print0 | grep -v target | xargs -0 -n1  basename src/generated/resources/META-INF/connectors)
+
+for C in $DIRS; do
+    echo $C
+done
